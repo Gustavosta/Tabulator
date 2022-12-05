73c3e10861b9fb5ef3ad623e6702fcb5536de6d8
Gustavo Santana <gustavo.santana6840@gmail.com>
2022-12-05 15:49:50 -0300

    Update project

--- config_example.py
+++ config_example.py
@@ -11,8 +11,8 @@ class Config:
     """
 
     TABNEWS_HOST = 'https://tabnews-8kgcqlch1-tabnews.vercel.app' # Use 'https://tabnews.com.br' for production environment
-    TABNEWS_EMAIL = ''
-    TABNEWS_PASSWORD = ''
+    TABNEWS_EMAIL = '' # Your TabNews email
+    TABNEWS_PASSWORD = '' # Your TabNews password
 
     OPENAI_KEY = '' # OpenAI API key, you can get one here: https://beta.openai.com/docs/api-reference/authentication
 
